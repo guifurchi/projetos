7af2b9b4614c045923345ec0133801f6a4904961
Guilherme Furchi <84292720+guifurchi@users.noreply.github.com>
2021-12-26 19:29:02 -0400

    Update processa_envio.php

--- app_send_mail/processa_envio.php
+++ app_send_mail/processa_envio.php
@@ -53,8 +53,8 @@ require "./vendor/phpmailer/phpmailer/src/SMTP.php";
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'guifurchi65@gmail.com';                     //SMTP username
-            $mail->Password   = 'gdfvpr9056';                               //SMTP password
+            $mail->Username   = 'teste@gmail.com';                     //SMTP username
+            $mail->Password   = '12345';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
             $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
